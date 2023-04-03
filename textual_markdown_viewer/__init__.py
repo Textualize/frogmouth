0972be0dc4b57c1f64fdee22913f05275505ccb4
Dave Pearson <davep@davep.org>
2023-04-03 13:41:14 +0100

    Add the usual package dunders

---
+++ textual_markdown_viewer/__init__.py
@@ -0,0 +1,9 @@
+"""A terminal-based Markdown document viewer, written in Textual."""
+
+__author__ = "Textualize, Inc"
+__copyright__ = "Copyright Textualize, Inc"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "dave@textualize.io"
+__version__ = "0.1.0"
+__licence__ = "MIT"
