86f2b3f4eafdd589c9a2a9b281c6a4c23f8ac6f9
Dave Pearson <davep@davep.org>
2023-04-05 09:56:32 +0100

    Initial support for history tracking
    Similar to the history stack in Textual itself, but this will track either a
    Path or a URL.

--- textual_markdown_viewer/widgets/viewer.py
+++ textual_markdown_viewer/widgets/viewer.py
@@ -1,11 +1,16 @@
 """The markdown viewer itself."""
 
+from __future__ import annotations
+
 from pathlib import Path
+from typing import Callable
 
 from httpx import AsyncClient, URL
 
 from textual.app import ComposeResult
 from textual.containers import VerticalScroll
+from textual.message import Message
+from textual.reactive import var
 from textual.widgets import Markdown
 
 from .. import __version__
@@ -17,8 +22,74 @@ Welcome to the Textual Markdown viewer!
 """
 
 
+class History:
+    """Holds the browsing history for the viewer."""
+
+    def __init__(self) -> None:
+        """Initialise the history object."""
+        self._history: list[Path | URL] = []
+        """The list that holds the history of locations visited."""
+        self._current: int = 0
+        """The current location."""
+
+    @property
+    def location(self) -> Path | URL | None:
+        """The current location in the history."""
+        try:
+            return self._history[self._current]
+        except IndexError:
+            return None
+
+    def remember(self, location: Path | URL) -> None:
+        """Remember a new location in the history.
+
+        Args:
+            location: The location to remember.
+        """
+        self._history.append(location)
+        self._current = len(self._history) - 1
+
+    def back(self) -> bool:
+        """Go back in the history.
+
+        Returns:
+            `True` if the location changed, `False` if not.
+        """
+        if self._current:
+            self._current -= 1
+            return True
+        return False
+
+    def forward(self) -> bool:
+        """Go forward in the history.
+
+        Returns:
+            `True` if the location changed, `False` if not.
+        """
+        if self._current < len(self._history) - 1:
+            self._current -= 1
+            return True
+        return False
+
+
 class Viewer(VerticalScroll, can_focus=True, can_focus_children=True):
     """The Markdown viewer class."""
+
+    _history: var[History] = var(History)
+    """The browsing history."""
+
+    class LocationChanged(Message):
+        """Message sent when the viewer location changes."""
+
+        def __init__(self, viewer: Viewer) -> None:
+            """Initialise the location changed message.
+
+            Args:
+                viewer: The viewer sending the message.
+            """
+            super().__init__()
+            self.viewer: Viewer = viewer
+            """The viewer that sent the message."""
 
     def compose(self) -> ComposeResult:
         """Compose the markdown viewer."""
@@ -44,16 +115,42 @@ class Viewer(VerticalScroll, can_focus=True, can_focus_children=True):
             # TODO: Lots of error handling.
             self.document.update(response.text)
 
-    async def visit(self, location: Path | URL) -> None:
+    async def visit(self, location: Path | URL, remember: bool = True) -> None:
         """Visit a location.
 
         Args:
             location: The location to visit.
+            remember: Should this visit be added to the history?
         """
+        # Based on the type of the location, load up the content.
         if isinstance(location, Path):
             await self.document.load(location)
         elif isinstance(location, URL):
             await self._remote_load(location)
         else:
             raise ValueError("Unknown location type passed to the Markdown viewer")
-        # TODO: add the location to the history.
+
+        # Remember the location in the history if we're supposed to.
+        if remember:
+            self._history.remember(location)
+
+        # Let anyone else know we've changed location.
+        self.post_message(self.LocationChanged(self))
+
+    async def _jump(self, direction: Callable[[], bool]) -> None:
+        """Jump in a particular direction within the history.
+
+        Args:
+            direction: A function that jumps in the desired direction.
+        """
+        if direction():
+            if self._history.location is not None:
+                await self.visit(self._history.location, False)
+
+    async def back(self) -> None:
+        """Go back in the viewer history."""
+        await self._jump(self._history.back)
+
+    async def forward(self) -> None:
+        """Go forward in the viewer history."""
+        await self._jump(self._history.forward)
