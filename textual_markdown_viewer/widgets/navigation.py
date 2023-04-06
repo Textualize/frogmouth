63a1b59cee43b19d2ba46aa306fe949a0d78fc06
Dave Pearson <davep@davep.org>
2023-04-06 16:16:32 +0100

    Add shift left and right as navigation pane switch keys

--- textual_markdown_viewer/widgets/navigation.py
+++ textual_markdown_viewer/widgets/navigation.py
@@ -38,8 +38,8 @@ class Navigation(Vertical):
     """
 
     BINDINGS = [
-        Binding("comma,a,ctrl+left", "previous_tab", "", show=False),
-        Binding("full_stop,d,ctrl+right", "next_tab", "", show=False),
+        Binding("comma,a,ctrl+left,shift+left", "previous_tab", "", show=False),
+        Binding("full_stop,d,ctrl+right,shift+right", "next_tab", "", show=False),
     ]
     """Bindings local to the navigation pane."""
 
