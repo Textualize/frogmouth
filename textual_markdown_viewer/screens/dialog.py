d4491c433d9bf1303f4bd2308fe70a99f0bf11d3
Dave Pearson <davep@davep.org>
2023-04-07 08:47:36 +0100

    Add a base message dialog screen
    This will be used to show information, and also as the base for things like
    error dialogs.

---
+++ textual_markdown_viewer/screens/dialog.py
@@ -0,0 +1,73 @@
+"""Provides modal dialog screens for the application."""
+
+from rich.text import TextType
+
+
+from textual.app import ComposeResult
+from textual.binding import Binding
+from textual.containers import Vertical, Center
+from textual.screen import ModalScreen
+from textual.widgets import Button, Static
+
+
+class ModalDialog(ModalScreen):
+    """Base modal dialog screen."""
+
+    DEFAULT_CSS = """
+    ModalDialog {
+        align: center middle;
+    }
+
+    ModalDialog > Vertical {
+        background: $panel;
+        min-width: 30%;
+        width: auto;
+        height: auto;
+        border: round $primary;
+    }
+
+    ModalDialog Static {
+        width: auto;
+    }
+
+    ModalDialog .spaced {
+        padding: 1;
+    }
+
+    ModalDialog #message {
+        border-top: solid $primary;
+        border-bottom: solid $primary;
+    }
+    """
+
+    BINDINGS = [
+        Binding("escape", "app.pop_screen", "", show=False),
+    ]
+
+    def __init__(self, title: TextType, message: TextType) -> None:
+        """Initialise the modal dialog.
+
+        Args:
+            title: The title for the dialog.
+            message: The message to show.
+        """
+        super().__init__()
+        self._title = title
+        self._message = message
+
+    def compose(self) -> ComposeResult:
+        """Compose the content of the modal dialog."""
+        with Vertical():
+            with Center():
+                yield Static(self._title, classes="spaced")
+            yield Static(self._message, id="message", classes="spaced")
+            with Center(classes="spaced"):
+                yield Button("OK")
+
+    def on_mount(self) -> None:
+        """Configure the dialog once the DOM has loaded."""
+        self.query_one(Button).focus()
+
+    def on_button_pressed(self) -> None:
+        """Handle the OK button being pressed."""
+        self.app.pop_screen()
