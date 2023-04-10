997789e2f0a48ae7161ee3b1817b946c3e9ef478
Dave Pearson <davep@davep.org>
2023-04-10 15:07:42 +0100

    Move dialogs into their own module
    While they are screens, they have a different purpose so let's lean into
    that.

---
+++ textual_markdown_viewer/dialogs/__init__.py
@@ -0,0 +1,5 @@
+"""Provides useful dialogs for the application."""
+
+from .dialog import ErrorDialog, InformationDialog
+
+__all__ = ["ErrorDialog", "InformationDialog"]
--- textual_markdown_viewer/screens/__init__.py
+++ textual_markdown_viewer/screens/__init__.py
@@ -1,6 +1,5 @@
 """The screens for the application."""
 
 from .main import Main
-from .dialog import ErrorDialog, InformationDialog
 
-__all__ = ["Main", "ErrorDialog", "InformationDialog"]
+__all__ = ["Main"]
