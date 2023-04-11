1dd201141f68495881dc336a07df9f6ff72fa22d
Dave Pearson <davep@davep.org>
2023-04-11 09:50:54 +0100

    Don't try and change directory after there was an error

--- textual_markdown_viewer/screens/main.py
+++ textual_markdown_viewer/screens/main.py
@@ -173,7 +173,8 @@ class Main(Screen):  # pylint:disable=too-many-public-methods
             self.app.push_screen(
                 ErrorDialog("Not a directory", f"{event.target} is not a directory.")
             )
-        await self.query_one(Navigation).jump_to_local_files(event.target)
+        else:
+            await self.query_one(Navigation).jump_to_local_files(event.target)
 
     def on_omnibox_history_command(self) -> None:
         """Handle being asked to view the history."""
