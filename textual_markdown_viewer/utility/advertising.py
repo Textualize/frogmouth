320875a5890e41abdef8b5a6d1ee050929ef8a7e
Dave Pearson <davep@davep.org>
2023-04-11 20:24:33 +0100

    Type the app branding constants as final

--- textual_markdown_viewer/utility/advertising.py
+++ textual_markdown_viewer/utility/advertising.py
@@ -1,15 +1,17 @@
 """Provides the 'branding' for the application."""
 
+from typing_extensions import Final
+
 from .. import __version__
 
-ORGANISATION_NAME = "textualize"
+ORGANISATION_NAME: Final[str] = "textualize"
 """The organisation name to use when creating namespaced resources."""
 
-PACKAGE_NAME = "textual-markdown-viewer"
+PACKAGE_NAME: Final[str] = "textual-markdown-viewer"
 """The name of the package."""
 
-APPLICATION_TITLE = "Textual Markdown Viewer"
+APPLICATION_TITLE: Final[str] = "Textual Markdown Viewer"
 """The title of the application."""
 
-USER_AGENT = f"{PACKAGE_NAME} v{__version__}"
+USER_AGENT: Final[str] = f"{PACKAGE_NAME} v{__version__}"
 """The user agent to use when making web requests."""
