5f82fd28e5c6e919542eb5f513fb602f93300158
Dave Pearson <davep@davep.org>
2023-04-26 14:32:57 +0100

    Make the Viewer 1fr now
    It was 3fr, to share space with the Navigation (which was 1fr), but now that
    that's docked and fixed width it's not required any more.

--- frogmouth/widgets/viewer.py
+++ frogmouth/widgets/viewer.py
@@ -97,7 +97,7 @@ class Viewer(VerticalScroll, can_focus=True, can_focus_children=True):
 
     DEFAULT_CSS = """
     Viewer {
-        width: 3fr;
+        width: 1fr;
         scrollbar-gutter: stable;
     }
     """
